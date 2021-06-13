491cd7ebf6e5c8c3ed4c9babd39f13b227a03435
thomasfermi <mario.theers@gmail.com>
2021-06-13 21:41:59 +0200

    CalibratedLaneDetector only estimates vp when lanes are straight

--- code/solutions/camera_calibration/calibrated_lane_detector.py
+++ code/solutions/camera_calibration/calibrated_lane_detector.py
@@ -26,7 +26,7 @@ def get_py_from_vp(u_i, v_i, K):
     return pitch, yaw
 
 class CalibratedLaneDetector(LaneDetector):
-    def __init__(self, calib_cut_v = 300, cam_geom=CameraGeometry(), model_path='./lane_segmentation_model.pth', 
+    def __init__(self, calib_cut_v = 200, cam_geom=CameraGeometry(), model_path='./lane_segmentation_model.pth', 
         encoder = 'efficientnet-b0', encoder_weights = 'imagenet'):
         # call parent class constructor
         super().__init__(cam_geom, model_path, encoder, encoder_weights)
@@ -44,36 +44,45 @@ class CalibratedLaneDetector(LaneDetector):
         self.calibration_success = False
     
     def _fit_line_v_of_u(self, probs):
-        # TODO: maybe we should make sure that degree 1 polynomial is a good fit here? 
-        # Otherwise the image shall not be used for calibration
         probs_flat = np.ravel(probs[self.calib_cut_v:, :])
         mask = probs_flat > 0.3
-        coeffs = np.polyfit(self.uv_grid[:,0][mask], self.uv_grid[:,1][mask], deg=1, w=probs_flat[mask])
-        return np.poly1d(coeffs)
+        coeffs, residuals, _, _, _ = np.polyfit(
+            self.uv_grid[:,0][mask], self.uv_grid[:,1][mask], deg=1, w=probs_flat[mask], full=True)
+        mean_residuals = residuals/len(self.uv_grid[:,0][mask])
+        #print(mean_residuals)
+        if mean_residuals < 15:
+            return np.poly1d(coeffs)
+        else:
+            return None
 
     def calibrate(self, image, mpl_axis=None):
         _, left_probs, right_probs = self.detect(image)
         line_left  = self._fit_line_v_of_u(left_probs)
         line_right = self._fit_line_v_of_u(right_probs)
+        if line_left is None or line_right is None:
+            return False
         vanishing_point = get_intersection(line_left, line_right)
-        if vanishing_point is not None:
-            if mpl_axis is not None:
-                self.visualize_vanishing_point(
-                    image, line_left, line_right, vanishing_point, mpl_axis)
-            u_i, v_i = vanishing_point
-            pitch, yaw = get_py_from_vp(u_i, v_i, self.cg.intrinsic_matrix)
-            self.add_to_pitch_yaw_history(pitch, yaw)
-        return None
+        if vanishing_point is None:
+            return False      
+          
+        if mpl_axis is not None:
+            self.visualize_vanishing_point(
+                image, line_left, line_right, vanishing_point, mpl_axis)
+        u_i, v_i = vanishing_point
+        pitch, yaw = get_py_from_vp(u_i, v_i, self.cg.intrinsic_matrix)
+        self.add_to_pitch_yaw_history(pitch, yaw)
+        return True
 
     def add_to_pitch_yaw_history(self, pitch, yaw):
         self.pitch_yaw_history.append([pitch, yaw])
-        if len(self.pitch_yaw_history) > 100:
+        if len(self.pitch_yaw_history) > 50:
             py = np.array(self.pitch_yaw_history)
             mean_pitch = np.mean(py[:,0])
             mean_yaw = np.mean(py[:,1])
             self.update_cam_geometry(mean_pitch, mean_yaw)
             self.calibration_success = True
             self.pitch_yaw_history = []
+            print("yaw, pitch = ", mean_yaw*180/np.pi, mean_pitch*180/np.pi)
 
     def update_cam_geometry(self, pitch, yaw):
         # TODO: CameraGeometry currently does not allow to set different yaw value :(
@@ -97,6 +106,7 @@ class CalibratedLaneDetector(LaneDetector):
         mpl_axis.scatter([u_i],[v_i], marker="o", s=100, color="r", zorder=10)
 
     def __call__(self, img):
+        self.calibrate(img)
         # only return lane lines once calibrated
         if self.calibration_success:
             return super().__call__(img)
